ccec5e7bbeb81c7cd47c4f205fcf7ef64772005d
CP-Rahul <rahulcp1401@gmail.com>
2023-12-15 18:15:40 +0530

    Added rate limiter

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const app = express();
 
+const { rateLimit } = require('express-rate-limit');
+
 const { ServerConfig } = require('./config');
 const apiRoutes = require('./routes');
 
+const limiter = rateLimit({
+	windowMs: 10 * 60 * 1000, 
+	limit: 50,
+})
+
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+
+app.use(limiter);
 
 app.use('/api', apiRoutes);
 
